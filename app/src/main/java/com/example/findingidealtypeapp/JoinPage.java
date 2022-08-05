0d23e3d0a0a47fb6fbc3bea2fdf8acdd3f43436c
조준희 <iopp3423@gmail.com>
2022-08-05 12:59:26 +0900

    [ADD] 로그인 페이지 아무 입력안하고 버튼 누를 때 validate 구현

--- app/src/main/java/com/example/findingidealtypeapp/JoinPage.java
+++ app/src/main/java/com/example/findingidealtypeapp/JoinPage.java
@@ -1,17 +1,42 @@
 package com.example.findingidealtypeapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
 public class JoinPage extends Fragment {
+
+    private EditText inputEmail, inputName, inputPassword, inputPasswordCheck;
+    private Button JoinButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.join_page, container, false);
+
+        inputEmail = rootView.findViewById(R.id.inputEmail); //
+        inputName = rootView.findViewById(R.id.inputName);
+        inputPassword = rootView.findViewById(R.id.inputPasswordOfJoin);
+        inputPasswordCheck= rootView.findViewById(R.id.inputPasswordCheck);
+
+        JoinButton.setOnClickListener(new View.OnClickListener() { // 회원가입 버튼 누를 때 이벤트
+            @Override
+            public void onClick(View v) {
+                String userEmail = inputEmail.getText().toString();
+                String userPassword = inputPassword.getText().toString();
+
+                System.out.println(userEmail);
+                System.out.println(userPassword);
+                Log.d("test", "test:" + userEmail);
+            }
+        });
+
 
         return rootView;
     }
--- app/src/main/java/com/example/findingidealtypeapp/LoginPage.java
+++ app/src/main/java/com/example/findingidealtypeapp/LoginPage.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -15,24 +16,27 @@ import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class LoginPage extends Fragment {
-    
+
     private EditText inputEmail, inputPassword;
     private Button loginButton;
+    private ViewGroup rootView;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.login_page, container, false);
+        rootView = (ViewGroup)inflater.inflate(R.layout.login_page, container, false);
         inputEmail = rootView.findViewById(R.id.inputEmailAddress);
         inputPassword = rootView.findViewById(R.id.inputPassword);
         loginButton = rootView.findViewById(R.id.loginButton);
 
         //로그인 버튼 눌렀을 때 수행
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() { // 로그인 버튼 눌렀을 때 이벤트
             @Override
             public void onClick(View v) {
+                validateCheck(); // 이메일, 비밀번호 유효성검사
                 String userEmail = inputEmail.getText().toString();
                 String userPassword = inputPassword.getText().toString();
+
 
                 System.out.println(userEmail);
                 System.out.println(userPassword);
@@ -41,4 +45,18 @@ public class LoginPage extends Fragment {
         });
         return rootView;
     }
+
+   public void validateCheck(){
+       if ( inputEmail.getText().toString().length() == 0 ) {
+           Toast.makeText(rootView.getContext(), "Email 입력하세요", Toast.LENGTH_SHORT).show();
+           inputEmail.requestFocus();
+           return;
+       }
+
+       if ( inputPassword.getText().toString().length() == 0 ) {
+           Toast.makeText(rootView.getContext(), "Password 입력하세요", Toast.LENGTH_SHORT).show();
+           inputPassword.requestFocus();
+           return;
+       }
+   }
 }
