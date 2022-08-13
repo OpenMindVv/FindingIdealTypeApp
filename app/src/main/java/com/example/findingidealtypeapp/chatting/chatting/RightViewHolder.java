dee188853fd0a0d453ec4c4851a193b32b10de16
ming-taro <hera002008@naver.com>
2022-08-13 23:26:19 +0900

    [ADD] 채팅방에 대화 상자 추가

---
+++ app/src/main/java/com/example/findingidealtypeapp/chatting/chatting/RightViewHolder.java
@@ -0,0 +1,23 @@
+package com.example.findingidealtypeapp.chatting.chatting;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.findingidealtypeapp.R;
+
+public class RightViewHolder extends RecyclerView.ViewHolder{
+
+    public TextView content;
+    public TextView time;
+
+    public RightViewHolder(@NonNull View itemView) {
+
+        super(itemView);
+
+        content = itemView.findViewById(R.id.content);
+        time = itemView.findViewById(R.id.time);
+    }
+}
---
+++ app/src/main/java/com/example/findingidealtypeapp/chatting/utility/Constants.java
@@ -0,0 +1,7 @@
+package com.example.findingidealtypeapp.chatting.utility;
+
+public class Constants {
+    public static final int LEFT_CONTENT = 0;
+    public static final int CENTER_CONTENT = 1;
+    public static final int RIGHT_CONTENT = 2;
+}
