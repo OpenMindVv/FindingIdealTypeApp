449a79f8a7403113e83b761c91241d98cbed248e
littleemm <lilysolar@naver.com>
2022-09-09 18:14:53 +0900

    [ADD] 메인페이지 뼈대 끝

--- app/src/main/java/com/example/findingidealtypeapp/mainpage/MainPage.java
+++ app/src/main/java/com/example/findingidealtypeapp/mainpage/MainPage.java
@@ -2,10 +2,12 @@ package com.example.findingidealtypeapp.mainpage;
 
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.findingidealtypeapp.R;
 
@@ -19,8 +21,22 @@ public class MainPage extends Fragment {
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.activity_user_list, container, false);
+
+        Button rabbitButton = (Button) view.findViewById(R.id.rabbit_button);
+        Button dogButton = (Button) view.findViewById(R.id.dog_button);
+        Button catButton = (Button) view.findViewById(R.id.cat_button);
+        Button dinosaurButton = (Button) view.findViewById(R.id.dinosaur_button);
+
+        rabbitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), CustomListFragment.class);
+                startActivity(intent);
+            }
+        });
+
+
 
         return view;
     }
