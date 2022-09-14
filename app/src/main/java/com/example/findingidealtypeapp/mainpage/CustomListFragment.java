242d2d570e3b00b7741e735a343ef2383064c30a
littleemm <lilysolar@naver.com>
2022-09-14 20:17:23 +0900

    [ADD] 오늘의 이상형 배너에 클릭리스너 추가

--- app/src/main/java/com/example/findingidealtypeapp/mainpage/CustomListFragment.java
+++ app/src/main/java/com/example/findingidealtypeapp/mainpage/CustomListFragment.java
@@ -1,10 +1,12 @@
 package com.example.findingidealtypeapp.mainpage;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
@@ -38,23 +40,16 @@ public class CustomListFragment extends Fragment {
 
         //data를 가져와서 어답터와 연결해 준다. 서버에서 가져오는게 대부분 이다.
         userList = new ArrayList<>();
-        userList.add(new User("JENNIERUBYJANE", R.drawable.jennie));
-        userList.add(new User("SOOYAAA___", R.drawable.jisoo));
-        userList.add(new User("ROSES_ARE_ROSIE", R.drawable.rose));
-        userList.add(new User("LALALALISA_M", R.drawable.lisa));
-        userList.add(new User("_FOR_YXXNG", R.drawable.kiyong));
+        userList.add(new User("JENNIERUBYJANE", R.drawable.jennie, "CAT"));
+        userList.add(new User("SOOYAAA___", R.drawable.jisoo, "DOG"));
+        userList.add(new User("ROSES_ARE_ROSIE", R.drawable.rose, "CAT"));
+        userList.add(new User("LALALALISA_M", R.drawable.lisa, "CAT"));
+        userList.add(new User("_FOR_YXXNG", R.drawable.kiyong, "DINOSAUR"));
 
         customListView = (ListView) rootView.findViewById(R.id.listView_custom);
         customAdapter = new CustomAdapter(getContext(),userList);
         customListView.setAdapter(customAdapter);
-        customListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
-                //각 아이템을 분간 할 수 있는 position과 뷰
-                String selectedItem = (String) view.findViewById(R.id.textView_name).getTag().toString();
-                Toast.makeText(getContext(), "Clicked: " + position +" " + selectedItem, Toast.LENGTH_SHORT).show();
-            }
-        });
+
 
         todayLayout = (ConstraintLayout) rootView.findViewById(R.id.today_layout);
         todayId = (TextView) rootView.findViewById(R.id.today_id);
@@ -66,11 +61,23 @@ public class CustomListFragment extends Fragment {
         todayLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.v("오늘의 이상형", todayId.toString());
+                OnClickShowAlert(rootView);
             }
         });
 
+
+
         return rootView;
+    }
+
+    public void OnClickShowAlert(View rootView) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(rootView.getContext());
+        builder.setTitle("오늘의 이상형");
+        builder.setMessage("JENNIERUBYJANE" +" 님은 " + "토끼상" + "입니다.");
+        builder.setPositiveButton("채팅하기", null);
+        builder.setNeutralButton("닫기", null);
+
+        builder.show();
     }
 }
 
@@ -79,10 +86,12 @@ public class CustomListFragment extends Fragment {
 class User {
     private String name;
     private int thumb_url;
+    private String animal;
 
-    public User(String name, int thumb_url) {
+    public User(String name, int thumb_url, String animal) {
         this.name = name;
         this.thumb_url = thumb_url;
+        this.animal = animal;
     }
 
     public String getName() {
@@ -92,4 +101,6 @@ class User {
     public int getThumb_url() {
         return thumb_url;
     }
+
+    public String getAnimal() { return animal; }
 }
